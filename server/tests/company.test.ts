ca63ea698a5e28d9f91ed4905a77ec2116941bd4
Ki2lian <64066124+Ki2lian@users.noreply.github.com>
2023-11-24 14:41:55 +0100

    test: Add unit tests for company modification and deletion

--- server/tests/company.test.ts
+++ server/tests/company.test.ts
@@ -4,6 +4,8 @@ import companyController from '../src/companies/company.controller';
 import companyMiddleware from '../src/middlewares/company.middleware';
 
 const companyServiceAddCompanyMock = jest.spyOn(companyService, 'addCompany');
+const companyServiceUpdateCompanyMock = jest.spyOn(companyService, 'updateCompany');
+const companyServiceDeleteCompanyMock = jest.spyOn(companyService, 'deleteCompany');
 const companyData = {
     name: 'Example Company',
     description: 'A sample description',
@@ -12,8 +14,14 @@ const companyData = {
     applicationDate: new Date(),
 };
 
+const updateCompanyData = {
+    ...companyData,
+};
+
 const createMockRequestResponse = () => {
     const req = {} as Request;
+    req.user = { payload: { userId: 1 } };
+    req.body = {};
     const res = {} as Response;
     res.status = jest.fn().mockReturnValue(res);
     res.json = jest.fn().mockReturnValue(res);
@@ -30,7 +38,6 @@ describe('Company', () => {
                 req.body = {
                     ...companyData,
                 };
-                req.user = { payload: { userId: 1 } };
 
                 companyServiceAddCompanyMock.mockResolvedValue(req.body);
                 await companyController.addCompany(req, res);
@@ -55,17 +62,63 @@ describe('Company', () => {
                 });
             });
         });
+        describe('Update Company', () => {
+            it('should update an existing company', async () => {
+                const { req, res } = createMockRequestResponse();
+                req.body = {
+                    ...updateCompanyData,
+                };
+                req.params = { id: '1' };
+
+                companyServiceUpdateCompanyMock.mockResolvedValue(req.body);
+                await companyController.updateCompany(req, res);
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(res.json).toHaveBeenCalledWith(updateCompanyData);
+            });
+            it('should return 404 if the company does not exist', async () => {
+                const { req, res } = createMockRequestResponse();
+                req.body = {
+                    ...updateCompanyData,
+                };
+                req.params = { id: '1' };
+
+                companyServiceUpdateCompanyMock.mockResolvedValue(null);
+
+                await companyController.updateCompany(req, res);
+                expect(res.status).toHaveBeenCalledWith(404);
+                expect(res.json).toHaveBeenCalledWith({ error: 'Entreprise non trouvée' });
+            });
+        });
+        describe('Delete Company', () => {
+            it('should delete an existing company', async () => {
+                const { req, res } = createMockRequestResponse();
+                req.params = { id: '1' };
+
+                companyServiceDeleteCompanyMock.mockResolvedValue(req.body);
+                await companyController.deleteCompany(req, res);
+                expect(res.status).toHaveBeenCalledWith(204);
+            });
+            it('should return 404 if the company does not exist', async () => {
+                const { req, res } = createMockRequestResponse();
+                req.params = { id: '1' };
+
+                companyServiceDeleteCompanyMock.mockResolvedValue(null);
+                await companyController.deleteCompany(req, res);
+
+                expect(res.status).toHaveBeenCalledWith(404);
+                expect(res.json).toHaveBeenCalledWith({ error: 'Entreprise non trouvée' });
+            });
+        });
     });
     describe('Middleware', () => {
-        describe('validateAddCompanyData', () => {
+        describe('validateCompanyData', () => {
             it('should call next() if data is valid', async () => {
                 const { req, res, next } = createMockRequestResponse();
                 req.body = {
                     ...companyData,
                 };
-                req.user = { payload: { userId: 1 } };
 
-                companyMiddleware.validateAddCompanyData(req, res, next);
+                companyMiddleware.validateCompanyData(req, res, next);
 
                 expect(next).toHaveBeenCalled();
             });
@@ -76,7 +129,7 @@ describe('Company', () => {
                     name: undefined,
                 };
 
-                companyMiddleware.validateAddCompanyData(req, res, next);
+                companyMiddleware.validateCompanyData(req, res, next);
 
                 expect(next).not.toHaveBeenCalled();
                 expect(res.status).toHaveBeenCalledWith(400);
@@ -89,7 +142,7 @@ describe('Company', () => {
                     website: 'invalid',
                 };
 
-                companyMiddleware.validateAddCompanyData(req, res, next);
+                companyMiddleware.validateCompanyData(req, res, next);
 
                 expect(next).not.toHaveBeenCalled();
                 expect(res.status).toHaveBeenCalledWith(400);
@@ -102,11 +155,24 @@ describe('Company', () => {
                     applicationDate: 'invalid',
                 };
 
-                companyMiddleware.validateAddCompanyData(req, res, next);
+                companyMiddleware.validateCompanyData(req, res, next);
 
                 expect(next).not.toHaveBeenCalled();
                 expect(res.status).toHaveBeenCalledWith(400);
                 expect(res.json).toHaveBeenCalledWith({ error: 'Date de candidature invalide' });
+            });
+            it('should return a 400 status if state is not valid', async () => {
+                const { req, res, next } = createMockRequestResponse();
+                req.body = {
+                    ...companyData,
+                    state: 'invalid',
+                };
+
+                companyMiddleware.validateCompanyData(req, res, next);
+
+                expect(next).not.toHaveBeenCalled();
+                expect(res.status).toHaveBeenCalledWith(400);
+                expect(res.json).toHaveBeenCalledWith({ error: "L'état de la candidature n'est pas valide" });
             });
         });
     });
